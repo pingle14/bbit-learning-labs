b928487ec2c9d7a5516e8732ad81d95b18aeeffb
Srujan Mupparapu <srujanmup@gmail.com>
2024-02-27 19:04:36 -0600

    finish part 2 publisher

--- Tech-Lab-On-Campus/Topic-Exchange/publish.py
+++ Tech-Lab-On-Campus/Topic-Exchange/publish.py
@@ -21,27 +21,21 @@ from solution.producer_sol import mqProducer  # pylint: disable=import-error
 def main(ticker: str, price: float, sector: str) -> None:
     
     # Implement Logic to Create Routing Key from the ticker and sector variable -  Step 2
-    #
-    #                       WRITE CODE HERE!!!
-    #
-
+    routingKey = f"stock.{ticker}.{sector}"
 
     producer = mqProducer(routing_key=routingKey,exchange_name="Tech Lab Topic Exchange")
 
 
     # Implement Logic To Create a message variable from the variable EG. "TSLA price is now $500" - Step 3
-    #
-    #                       WRITE CODE HERE!!!
-    #
-    
+    message = f"{ticker} price is now ${price}"
     
     producer.publishOrder(message)
 
 if __name__ == "__main__":
 
     # Implement Logic to read the ticker, price and sector string from the command line and save them - Step 1
-    #
-    #                       WRITE CODE HERE!!!
-    #
+    ticker = str(sys.argv[1])
+    price = float(sys.argv[2])
+    sector = str(sys.argv[3])
 
     sys.exit(main(ticker,price,sector))
